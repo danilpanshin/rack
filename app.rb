820a8c83cf0b1af2401f40a904e12113ffb4f838
danilpanshin <danilpanshin@gmail.com>
2018-04-02 01:57:30 +0300

    не работает, а должно

--- app.rb
+++ app.rb
@@ -5,23 +5,31 @@ class App
   def call(env)  
     
     @request = Rack::Request.new(env)
-    @user_format = (Rack::Utils.parse_nested_query(@request.query_string)).values.join.split(",")
-    
-    case @request.path 
-    when  "/time"
-      handle_one_request
-    else
-      not_found
-    end     
+    @user_format = Rack::Utils.parse_nested_query(@request.query_string).values.join.split(",")
     
     [@status, headers, @body]
   end
 
   private 
 
+  def request
+
+    case @request.path 
+    when  "/time"
+      handle_one_request
+    else
+      not_found
+    end     
+
+  end
+
   def handle_one_request    
     @body = Time.new(@user_format)
-    @status = 200
+    if @body.acceptably?
+      @status = 200
+    else
+      @status = 400
+    end    
   end
 
 
